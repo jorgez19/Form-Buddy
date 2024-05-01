8e6a9178134b75bbf0ffd85d9f4c13691742c8f6
Jorge <jorgezavala14@hotmail.com>
2024-05-01 17:39:17 -0600

    Changed name to Doc Buddy

--- Hello.py
+++ Hello.py
@@ -5,7 +5,7 @@ st.set_page_config(
     page_icon="👋",
 )
 st.markdown("↖️ Note: Use arrow menu in top-left corner")
-st.write("# Welcome to Form Buddy! 👋")
+st.write("# Welcome to Doc Buddy! 👋")
 
 st.sidebar.success("Select a demo above.")
 
@@ -16,13 +16,13 @@ st.markdown(
 The current waiting time to process a Form I-485 is a staggering 29 months*. Families are separated, dreams are put on hold, and the system is overwhelmed. But there's hope on the horizon.
 
 ### Our Solution
-Form Buddy harnesses the power of artificial intelligence to revolutionize the immigration process. By utilizing Google's Gemini API, I've created a tool that reads and analyzes I485 forms with lightning speed and unparalleled accuracy.
+Doc Buddy harnesses the power of artificial intelligence to revolutionize the immigration process. By utilizing Google's Gemini API, I've created a tool that reads and analyzes I485 forms with lightning speed and unparalleled accuracy.
 
 ### How It Works
-In the **Applicant** Tab, users can submit their forms with ease. Form Buddy will not only review your submission but also offer valuable suggestions and alert you to any mistakes you may have made. Say goodbye to endless waiting and hello to a streamlined process.
+In the **Applicant** Tab, users can submit their forms with ease. Doc Buddy will not only review your submission but also offer valuable suggestions and alert you to any mistakes you may have made. Say goodbye to endless waiting and hello to a streamlined process.
 
 ### Empowering USCIS Agents
-But our impact doesn't stop there. In the **USCIS Agent** tab, I demonstrate how USCIS agents can utilize Form Buddy to process forms faster and more efficiently. Think of it as your trusted assistant, guiding you through each step with precision.
+But our impact doesn't stop there. In the **USCIS Agent** tab, I demonstrate how USCIS agents can utilize Doc Buddy to process forms faster and more efficiently. Think of it as your trusted assistant, guiding you through each step with precision.
 
 *Wait times were taken from https://egov.uscis.gov/processing-times/ using the Salt Lake City office.
 
